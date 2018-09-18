e207cfc52dfc8f4e1d3d7a88583e927553bb45d5
doombubbles <obsidiantoolzg@gmail.com>
2018-09-18 09:29:10 -0700

    Finish class comments documentation

--- src/MainMenuScreen.java
+++ src/MainMenuScreen.java
@@ -1,17 +1,9 @@
 /**
  * MainMenuScreen.java
- * Assignment: Final Project - Studiam
- * Purpose:Studiam would be a tool used to help memorize specific vocabulary for classes and
- * practice for tests, particularly for learning complex languages like Latin. Users would
- * be able to type up or import vocabulary lists for classes into the software and practice
- * with the material. The software would randomly generate mock-up practice tests with all
- * of the content, optionally timed, and we give immediate feedback. It would also have the
- * tools to make the test as “smart” as possible, i.e. accepting multiple potential answers,
- * common spelling mistakes, punctuation and capitalization accepted. The focus would also
- * be on user-friendliness, and much of the development would be focused on making an intuitive,
- * aesthetically pleasing user interface that could be piloted well even without much experience.
  *
- * @version 06/19/18
+ * The Main {@link Screen}. 'Nuff said
+ *
+ * @version 09/18/18
  */
 
 import javax.swing.*;
--- src/QuizFile.java
+++ src/QuizFile.java
@@ -1,17 +1,9 @@
 /**
  * QuizFile.java
- * Assignment: Final Project - Studiam
- * Purpose:Studiam would be a tool used to help memorize specific vocabulary for classes and
- * practice for tests, particularly for learning complex languages like Latin. Users would
- * be able to type up or import vocabulary lists for classes into the software and practice
- * with the material. The software would randomly generate mock-up practice tests with all
- * of the content, optionally timed, and we give immediate feedback. It would also have the
- * tools to make the test as “smart” as possible, i.e. accepting multiple potential answers,
- * common spelling mistakes, punctuation and capitalization accepted. The focus would also
- * be on user-friendliness, and much of the development would be focused on making an intuitive,
- * aesthetically pleasing user interface that could be piloted well even without much experience.
  *
- * @version 06/19/18
+ * A file on the harddrive that is a valid Quiz
+ *
+ * @version 09/18/18
  */
 
 import java.io.File;
@@ -44,7 +36,7 @@ public class QuizFile extends File {
         output.println("quizScore = " + score.toString());
     }
 
-    //method to read the quiz file and turn it into a quiz class
+    //reads the quiz file and turn it into a quiz class
     public Quiz createQuiz() {
         Scanner scanner;
         try {
